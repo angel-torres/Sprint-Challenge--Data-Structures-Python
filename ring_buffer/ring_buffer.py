a50f3a617c8b7274fad46ff8f3f486ecbafac795
angel-torres <alas4224@gmail.com>
2019-11-15 12:19:16 -0600

    finished ring_buffer.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,29 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    # make a while loop
+    if self.current == self.capacity:
+        self.current = 0
+    self.storage[self.current] = item
+    self.current += 1    
 
   def get(self):
-    pass
+    filtered_list = []
+    for item in self.storage:
+        if item != None:
+            filtered_list.append(item)
+    return filtered_list
+
+rb = RingBuffer(5)
+
+#print(rb.storage, "< ------- INITIAL STORAGE")
+#print(rb.append(3))
+#print(rb.append(4))
+#print(rb.append(5))
+#print(rb.get())
+#print(rb.storage, "< ------- STORAGE")
+#print(rb.append(6))
+#print(rb.append(8))
+#print(rb.append(7))
+#print(rb.storage, "< ------- STORAGE")
+
